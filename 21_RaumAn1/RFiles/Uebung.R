f12f557d8c4c4c263e63305e9ffd1c984d54f45f
Ratnaweera Nils <rata@zhaw.ch>
2020-11-20 16:48:32 +0100

    stash

--- 21_RaumAn1/RFiles/Uebung.R
+++ 21_RaumAn1/RFiles/Uebung.R
@@ -47,11 +47,10 @@ wasser <- st_transform(wasser, 2056)
 
 wasser
 
-## -- Aufgabe 5 -- ##
+## -- Aufgabe 5: Spatialjoin -- ##
 
 wasser_skelett <- st_join(wasser,skelettgehalt)
 wasser_skelett
-
 
 
 wasser_skelett %>%
@@ -65,11 +64,11 @@ wasser_2km <- st_buffer(wasser,2000)
 
 ggplot(wasser_2km) + geom_sf(fill = "blue")
 
-library(fasterize)
 
-raster_template <- raster(extent(skelettgehalt), resolution = 1000)
+raster_template <- raster(extent(skelettgehalt), crs = st_crs(skelettgehalt)$proj4string, resolution = 1000)
 
-skelett_raster <- fasterize(skelettgehalt,raster_template,field = "SKELETT")
+
+skelett_raster <- rasterize(skelettgehalt,raster_template,field = "SKELETT")
 
 ggplot() + 
   geom_stars(data = st_as_stars(skelett_raster)) +
