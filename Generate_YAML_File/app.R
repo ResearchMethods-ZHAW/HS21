cb505a74664d610e23cdda6fba366511c2938e58
Ratnaweera Nils <rata@zhaw.ch>
2020-11-23 23:00:11 +0100

    doings some crazy stuff

--- Generate_YAML_File/app.R
+++ Generate_YAML_File/app.R
@@ -8,29 +8,64 @@ rmdfiles <- read.csv(here("rmdfiles.csv")) # make this an input (todo)
 folders <- unique(rmdfiles$Folder)
 
 ui <- fluidPage(
-    
-    h1("Generate _bookdown.yaml"),
-    div("This application helps create the List of RMD files in the '_bookdown.yaml' file."),
-    
-    # h2("Choose Entire Folder"),
-    fluidRow(
-        column(12, selectizeInput("variable", "Choose an entire Folder (or multiple Folders)",folders, multiple = TRUE))
+    tags$head(
+        tags$style(HTML("
+      
+      .dataTables_filter {visibility: hidden;}
+
+    "))
     ),
     
+    titlePanel("Generate the yaml-File"),
     
-    h2("Generatate '_bookdown.yaml'"),
-    fluidRow(
-        column(2, actionButton("writeyaml", "Write YAML File"))
+    sidebarLayout(
+        
+        sidebarPanel(
+            fluidRow(
+                selectInput("variable", 
+                            "Choose one or more topics to include",
+                            folders, 
+                            multiple = TRUE,
+                            selectize = FALSE,
+                            # selected = folders,
+                            size = 10),
+                textInput("yamlfile", "Name of the YAML",value = "_bookdown.yml"),
+                actionButton("writeyaml", "Write to YAML")
+                
+            )
+        ),
+        mainPanel(
+            tabsetPanel(
+                tabPanel("Select Files",
+                    fluidRow(
+                        column(12, DT::dataTableOutput('x3'))
+                    )
+                ),
+                tabPanel("Check result",
+                         column(12, div("Files to render:")),
+                         column(12, shiny::htmlOutput('x4'))
+                         ),
+                tabPanel("Readme",
+                         helpText("I created this app to simplify the selection and deselection of rmd-files to be included in the '_bookdown.yaml'-file. The idea is, that we add ALL the files with their respective folder names in a csv (csvfiles.csv) and then use this file to individually select / deselect files and folders to inlcude"),
+                         helpText("Once the files / folders have been selected, click on 'write to YAML' to export the resulting list into the '_bookdown.yaml-file'")
+                         )
+            )
+            
+        )
+        
     ),
     
-    h2("Choose individual files"),
-    fluidRow(
-        # column(2, textInput("yamlfile", "Name of the YAML",value = "_bookdown.yml")), # This has no effect yet (todo)
-        # column(3, verbatimTextOutput('x4')),
-        column(4, DT::dataTableOutput('x3'))
-    )
-    
+   
+   
 )
+
+
+
+
+
+
+
+
 
 
 server <- shinyServer(function(input, output, session) {
@@ -43,7 +78,7 @@ server <- shinyServer(function(input, output, session) {
     })
     
     
-    output$x3 = DT::renderDataTable(rmdfiles, server = TRUE,options = list(pageLength = 100),selection = list(target = "row",selected = rowids()))
+    output$x3 = DT::renderDataTable(rmdfiles, server = TRUE,filter = "top",options = list(pageLength = 100),selection = list(target = "row",selected = rowids()))
     
     rmdfiles2 <- reactive({
         rmdfiles_sel <- rmdfiles[rownames(rmdfiles) %in% input$x3_rows_selected,]
@@ -55,7 +90,7 @@ server <- shinyServer(function(input, output, session) {
         
     })
     
-    output$x4 <- renderPrint(rmdfiles2())
+    output$x4 <- renderUI(HTML(c("- ", paste(rmdfiles2(),collapse =  "<br/> - "))))
     
     observeEvent(input$writeyaml,{
         bookdown_yaml <- read_yaml(here("_bookdown.yml"))
