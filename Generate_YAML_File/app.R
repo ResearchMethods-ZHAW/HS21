11419ff8414c4413c943f6f8924ffacee5441994
Ratnaweera Nils <rata@zhaw.ch>
2020-11-30 13:29:20 +0100

    changed some things in the app

--- Generate_YAML_File/app.R
+++ Generate_YAML_File/app.R
@@ -2,104 +2,122 @@ library(shiny)
 library(DT)
 library(yaml)
 library(here)
-
+library(diffr)
+library(purrr)
 rmdfiles <- read.csv(here("rmdfiles.csv")) # make this an input (todo)
 names(rmdfiles)[1] <- "Folder"
+
+
+rmdfiles_list <-rmdfiles %>%
+  split(.$Folder) %>%
+  imap(function(df, folder){
+    val <- paste(df$Folder,df$File,sep = "/")
+    names(val) <- df$File
+    val
+  })
+
+
+
 
 folders <- unique(rmdfiles$Folder)
 
 ui <- fluidPage(
-    tags$head(
-        tags$style(HTML("
-      
-      .dataTables_filter {visibility: hidden;}
 
-    "))
-    ),
+  
+  titlePanel("Generate the yaml-File"),
+  
+  # sidebarLayout(
     
-    titlePanel("Generate the yaml-File"),
-    
-    sidebarLayout(
-        
-        sidebarPanel(
-            fluidRow(
-                selectInput("variable", 
-                            "Choose one or more topics to include",
-                            folders, 
-                            multiple = TRUE,
-                            selectize = FALSE,
-                            # selected = folders,
-                            size = 10),
-                textInput("yamlfile", "Name of the YAML",value = "_bookdown.yml"),
-                actionButton("writeyaml", "Write to YAML")
-                
-            )
+    # sidebarPanel(
+    #   fluidRow(
+    #     textInput("yamlfile", "Name of the YAML",value = "_bookdown.yml")
+    #   )
+    # ),
+    mainPanel(
+      tabsetPanel(
+ 
+        tabPanel("1. Select Files",
+                 column(6,selectInput("selector", 
+                                      "Choose one or more topics to include",
+                                      names(rmdfiles_list), 
+                                      multiple = TRUE,
+                                      selectize = FALSE,
+                                      selected = names(rmdfiles_list)[1],
+                                      size = length(rmdfiles_list),width = "100%")),
+                 column(6, fluidRow(uiOutput("fileselector")))
+                 
         ),
-        mainPanel(
-            tabsetPanel(
-                tabPanel("Select Files",
-                    fluidRow(
-                        column(12, DT::dataTableOutput('x3'))
-                    )
-                ),
-                tabPanel("Check result",
-                         column(12, div("Files to render:")),
-                         column(12, shiny::htmlOutput('x4'))
-                         ),
-                tabPanel("Readme",
-                         helpText("I created this app to simplify the selection and deselection of rmd-files to be included in the '_bookdown.yaml'-file. The idea is, that we add ALL the files with their respective folder names in a csv (csvfiles.csv) and then use this file to individually select / deselect files and folders to inlcude"),
-                         helpText("Once the files / folders have been selected, click on 'write to YAML' to export the resulting list into the '_bookdown.yaml-file'")
-                         )
-            )
-            
+
+        tabPanel("2. Check difference and export",
+                wellPanel(
+                  actionButton("writeyaml", "Write to YAML"),
+                  shiny::div("This writes the changes to '_output.yaml' and restarts the app")
+                  ),
+                 h3("Check the difference betweeen files"),
+                 diffrOutput("diff")
+        ),
+        tabPanel("Readme",
+                 helpText("I created this app to simplify the selection and deselection of rmd-files to be included in the '_bookdown.yaml'-file. The idea is, that we add ALL the files with their respective folder names in a csv (csvfiles.csv) and then use this file to individually select / deselect files and folders to inlcude"),
+                 helpText("Once the files / folders have been selected, click on 'write to YAML' to export the resulting list into the '_bookdown.yaml-file'")
         )
-        
-    ),
+      )
+      
+    )
     
-   
-   
+  # ),
+  
+  
+  
 )
 
 
 
-
-
-
-
-
-
-
 server <- shinyServer(function(input, output, session) {
-    
-    # server-side processing
-    output$value <- renderText({rownames(rmdfiles)[which(rmdfiles$Folder %in% input$variable)]})
-    
-    rowids <- reactive({
-        rownames(rmdfiles)[which(rmdfiles$Folder %in% input$variable)]
+  
+  bookdown_yaml <- read_yaml(here("_bookdown.yml"))
+  
+  folders_reactive <- reactive({
+    map(input$selector, function(x){rmdfiles_list[[x]]}) %>% 
+      magrittr::set_names(input$selector)
     })
-    
-    
-    output$x3 = DT::renderDataTable(rmdfiles, server = TRUE,filter = "top",options = list(pageLength = 100),selection = list(target = "row",selected = rowids()))
-    
-    rmdfiles2 <- reactive({
-        rmdfiles_sel <- rmdfiles[rownames(rmdfiles) %in% input$x3_rows_selected,]
-        
-        rmds_torender <- paste(rmdfiles_sel$Folder, rmdfiles_sel$File, sep = "/")
-        rmds_torender <- c("index.Rmd",rmds_torender)
-        
-        rmds_torender
-        
+  folders_reactive_unlist <- reactive({unlist(folders_reactive())})
+
+
+  output$x4 <- renderUI(HTML(c("- ", paste(folders_reactive_unlist(),collapse =  "<br/> - "))))
+  
+  
+  output$fileselector <- renderUI({
+    selectInput("fileselector","Remove specific Files from the chosen topic(s)", 
+                choices = folders_reactive(),selected = folders_reactive_unlist(),multiple = TRUE,
+                selectize = FALSE,
+                size = length(folders_reactive_unlist())+length(folders_reactive()),
+                width = "100%")
     })
+  
+  observeEvent(input$writeyaml,{
     
-    output$x4 <- renderUI(HTML(c("- ", paste(rmdfiles2(),collapse =  "<br/> - "))))
+    # bookdown_yaml <- read_yaml(here("_bookdown.yml"))
     
-    observeEvent(input$writeyaml,{
-        bookdown_yaml <- read_yaml(here("_bookdown.yml"))
-        bookdown_yaml$rmd_files <- rmdfiles2()
-        write_yaml(bookdown_yaml, here("_bookdown.yml"))
-        
+    bookdown_yaml$rmd_files <- c("index.Rmd",input$fileselector)
+    write_yaml(bookdown_yaml, here("_bookdown.yml"))
+    
+    session$reload()
+  })
+  
+    output$diff <- renderDiffr({
+      
+      # bookdown_yaml <- read_yaml(here("_bookdown.yml"))
+
+      bookdown_yaml$rmd_files <- c("index.Rmd",input$fileselector)
+      
+      file1 = here("_bookdown.yml")
+
+      file2 = tempfile(fileext = ".yml")
+      write_yaml(bookdown_yaml, file2)
+      diffr(file1, file2, before = "before", after = "after")
     })
-    
+  
+ 
 })
 
 
