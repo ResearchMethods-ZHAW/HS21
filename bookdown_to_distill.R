f6f0e1b7a197faf17a5315072c16bc0ffd5a3fd7
Nils Ratnaweera <rata@zhaw.ch>
2021-08-25 08:30:06 +0200

    updating script

--- bookdown_to_distill.R
+++ bookdown_to_distill.R
@@ -19,10 +19,10 @@ map_dfr(mydirs, function(x){
   if(length(abstract) != 1){
     print(paste("folder",x,"does not have an abstract"))
     title <- NA
-  }  else{
-    rl <- read_lines(abstract) 
-    title <- rl[str_detect(rl, "^#")]
-  }
+    }  else{
+      rl <- read_lines(abstract) 
+      title <- rl[str_detect(rl, "^#")]
+    }
   
   
   
@@ -48,169 +48,27 @@ mydf <- mydirs %>%
 
 
 mydf %>%
-  # head(6) %>%
   select(file,name,name2, tag) %>%
   pmap(function(file,name,name2, tag){
-    newdir <- paste0("_",str_to_lower(name2))
+    newdir <- paste0("_",str_to_lower(name))
     newdir
     if(!dir.exists(newdir)){dir.create(newdir)}
     
     
-    subdir <- paste(name, str_split(basename(file), "\\.")[[1]][1], sep = "_")
+   
+    
+    
+    subdir <- str_split(basename(file), "\\.")[[1]][1]
     subdirpath <- file.path(newdir, subdir)
     if(!dir.exists(subdirpath)){dir.create(subdirpath)}
     
-    rl <- read_lines(file)
+    # rl <- read_lines(file)
     
-    rl[str_detect(rl, "readLines")] <- "```"
+    # header <- c(
+    #   "---"
+    # )
     
-
-    title <- rl[str_detect(rl, "^#")][1]
-    
-    title <- str_extract(title, "\\w+")
-    yaml_header <- list(title = title, output = "distill::distill_article", categories =  c(name, name2)) %>%
-      yaml::as.yaml()
-    
-    head <- c(
-      "---",
-      yaml_header,
-      "---",
-      "",
-      "```{r echo = FALSE}",
-      "",
-      "```"
-    )
-    
-    rl <- c(head, rl)
-    
-    
-    
-    
-    paths <- str_match(rl, "([\\w\\/]+\\.(R|shp|csv|tif|gpkg|txt|png))")[,2]
-    
-    paths <- paths[!is.na(paths)]
-    
-    paths_new <- file.path("bookdown_files",paths)
-    
-    paths_new <- paths_new[file.exists(paths_new)]
-    
-    file_basename <- basename(paths_new)
-    
-    file.copy(paths_new, subdirpath)
-    
-    paths
-    # if(length(paths_new)>0){
-    #   map()
-    # }
-    rl <- str_replace(rl, '([\\w\\/]+\\/(\\w+)\\.(R|shp|csv|tif|gpkg|txt|png))','\\2.\\3')
-    
-    write_lines(rl, file.path(subdirpath, "index.Rmd"))
+    list(title = subdir) 
     
     
   })
-
-
-distill_dirs <- list.dirs(recursive = FALSE)
-distill_dirs <- distill_dirs[str_detect(basename(distill_dirs), "^_")]
-
-library(knitr)
-# render all rmd files in distill
-distill_dirs %>%
-  map(~list.files(.x, "index.Rmd", recursive = TRUE, full.names = TRUE)) %>%
-  unlist() %>%
-  # head(1) %>%
-  map(function(x){
-    tryCatch(rmarkdown::render(x), error = function(x){"couldn't render"})
-
-  })
-
-
-## Looks for code chunks that dont have an empty line before
-distill_dirs %>%
-  map(function(x){
-    list.files(path = x, pattern = ".Rmd", full.names = TRUE, recursive = TRUE)
-  }) %>%
-  unlist() %>%
-  # head(2) %>%
-  map_dfr(function(x){
-    print(paste("file", x))
-    
-    rl <- read_lines(x)
-    
-    code_chunks <- str_detect(rl, "```\\{r\\}")
-    nonempty <- lag(rl != "")
-    
-    lineses <- which(code_chunks & nonempty)
-    
-    tibble(file = x, lines = paste(lineses, collapse = ", "), n = length(lineses))
-}) %>%
-  filter(n)
-
-# looks for code chunks with a specific text
-distill_dirs %>%
-  map(function(x){
-    list.files(path = x, pattern = ".Rmd", full.names = TRUE, recursive = TRUE)
-  }) %>%
-  unlist() %>%
-  # head(2) %>%
-  map_dfr(function(x){
-    print(paste("file", x))
-    
-    rl <- read_lines(x)
-    
-    findings <- str_match(rl, "error = TRUE")[,1]
-
-
-    tibble(file = x, findings = findings)
-  }) 
-
-
-
-
-
-
-mydf %>%
-  group_by(name2, name) %>%
-  summarise() %>%
-  split(.$name2) %>%
-  imap(function(x,y){
-    x <- x$name
-    
-    
-    #### creates the yaml- menu header for _site.yml
-    li <- c(
-      glue('- text: "',{y},'"'),
-      '  menu:',
-      c(t(cbind(paste0('  - text: "',x,'"'),paste0('    href: ',x,'.html'))))
-    )
-    
-    # write_lines(li,"test.yaml",append = TRUE)
-    
-    
-   
-    })
-
-
-
-# file.remove(paste0(mydf$name, ".Rmd"))
-map(mydf$name2, function(x){
-  
-  
-  head <- c(
-    '---',
-    glue('title: "{x}"'),
-    glue('listing: {str_to_lower(x)}'),
-    '---'
-  )
-  
-  
-  write_lines(head, glue("{x}.Rmd"))
-  
-  
-})
-
-
-
-
-
-
