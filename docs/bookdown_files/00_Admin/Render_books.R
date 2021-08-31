d83888d05c363d59370aeed8cefd0812338c5e15
Nils <rata@zhaw.ch>
2021-08-31 15:15:20 +0200

    rm docs content

--- docs/bookdown_files/00_Admin/Render_books.R
+++
@@ -1,9 +0,0 @@
-library(bookdown)
-library(rsconnect)
-
-
-bookdown::render_book("index.Rmd",bookdown::gitbook(split_by = "section+number"),config_file = "_bookdown.yml")
-
-
-
-rsconnect::deploySite(siteName = "ResearchMethodsUebung")
