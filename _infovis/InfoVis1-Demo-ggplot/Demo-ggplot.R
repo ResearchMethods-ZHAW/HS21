3a8c124d3a1f9d50f7410489e950339d3db8401a
Nils <rata@zhaw.ch>
2021-10-20 09:08:31 +0200

    updated

--- _infovis/InfoVis1-Demo-ggplot/Demo-ggplot.R
+++ _infovis/InfoVis1-Demo-ggplot/Demo-ggplot.R
@@ -3,44 +3,47 @@ library(readr)
 library(lubridate)
 library(dplyr)
 library(ggplot2)
+library(tidyr)
 
-temperature <- read_csv("/home/nils/ownCloud/Lehre/Master/ResearchMethods/2021_HS/git_repos/datasets/infovis/temperature_SHA_ZER.csv",
-                   col_types = cols(
-                     col_factor(levels = NULL),    
-                     col_datetime(),
-                     col_double()
-                   )
-                  )
+# Wir können den Datensatz direkt über den URL einladen oder aber ihr nutzt den 
+# URL um den Datensatz lokal bei euch abzuspeichern und wie gewohnt einzulesen
+temperature <- read_csv("https://github.com/ResearchMethods-ZHAW/datasets/raw/main/infovis/temperature_SHA_ZER.csv")
 
 
-plot(temperature$time, temperature$temp)
+plot(temperature$time, temperature$SHA, type = "l", col = "red")
+lines(temperature$time, temperature$ZER, col = "blue")
+
 
 # Datensatz: "temperature" | Beeinflussende Variabeln: "time" und "temp"
-ggplot(data = temperature, mapping = aes(time,temp))             
+ggplot(data = temperature, mapping = aes(time,SHA))             
 
-ggplot(data = temperature, mapping = aes(time,temp)) +         
+ggplot(data = temperature, mapping = aes(time,SHA)) +         
   # Layer: "geom_point" entspricht Punkten in einem Scatterplot 
   geom_point()                                    
 
-ggplot(temperature, aes(time,temp)) +
+ggplot(temperature, aes(time,SHA)) +
   geom_point()
 
-ggplot(temperature, aes(time,temp, colour = stn)) +
+
+temperature_long <- pivot_longer(temperature, -time, names_to = "station", values_to = "temp")
+
+
+ggplot(temperature_long, aes(time,temp, colour = station)) +
   geom_point()
 
-ggplot(temperature, aes(time,temp, colour = stn)) +
+ggplot(temperature_long, aes(time,temp, colour = station)) +
   geom_point()+
   geom_line()
 
 
-ggplot(temperature, aes(time,temp, colour = stn)) +
+ggplot(temperature_long, aes(time,temp, colour = station)) +
   geom_line() +
   labs(x = "Zeit",
        y = "Temperatur in Grad C°", 
        title = "Temperaturdaten Schweiz",
        subtitle = "2001 bis 2002")
 
-ggplot(temperature, aes(time,temp, colour = stn)) +
+ggplot(temperature_long, aes(time,temp, colour = station)) +
   geom_line() +
   labs(x = "Zeit",
        y = "Temperatur in Grad C°", 
@@ -49,7 +52,7 @@ ggplot(temperature, aes(time,temp, colour = stn)) +
   scale_y_continuous(limits = c(-30,30))    # y-Achsenabschnitt bestimmen
 
 
-ggplot(temperature, aes(time,temp, colour = stn)) +
+ggplot(temperature_long, aes(time,temp, colour = station)) +
   geom_line() +
   labs(x = "Zeit",
        y = "Temperatur in Grad C°", 
@@ -60,7 +63,7 @@ ggplot(temperature, aes(time,temp, colour = stn)) +
                    date_labels = "%b")
 
 
-ggplot(temperature, aes(time,temp, colour = stn)) +
+ggplot(temperature_long, aes(time,temp, colour = station)) +
   geom_line() +
   labs(x = "Zeit",
        y = "Temperatur in Grad C°", 
@@ -73,7 +76,7 @@ ggplot(temperature, aes(time,temp, colour = stn)) +
 
 theme_set(theme_classic())
 
-ggplot(temperature, aes(time,temp, colour = stn)) +
+ggplot(temperature_long, aes(time,temp, colour = station)) +
   geom_line() +
   labs(x = "Zeit",
        y = "Temperatur in Grad C°", 
@@ -82,10 +85,10 @@ ggplot(temperature, aes(time,temp, colour = stn)) +
   scale_y_continuous(limits = c(-30,30)) +
   scale_x_datetime(date_breaks = "3 months", 
                    date_labels = "%b") +
-  facet_wrap(stn~.)
+  facet_wrap(station~.)
 
 
-ggplot(temperature, aes(time,temp, colour = stn)) +
+ggplot(temperature_long, aes(time,temp, colour = station)) +
   geom_line() +
   labs(x = "Zeit",
        y = "Temperatur in Grad C°", 
@@ -94,10 +97,10 @@ ggplot(temperature, aes(time,temp, colour = stn)) +
   scale_y_continuous(limits = c(-30,30)) +
   scale_x_datetime(date_breaks = "3 months", 
                    date_labels = "%b") +
-  facet_wrap(~stn,ncol = 3) +
+  facet_wrap(~station,ncol = 1) +
   theme(legend.position="none")
 
-p <- ggplot(temperature, aes(time,temp, colour = stn)) +
+p <- ggplot(temperature_long, aes(time,temp, colour = station)) +
   geom_line() +
   labs(x = "Zeit",
        y = "Temperatur in Grad C°", 
@@ -106,7 +109,7 @@ p <- ggplot(temperature, aes(time,temp, colour = stn)) +
   scale_y_continuous(limits = c(-30,30)) +
   scale_x_datetime(date_breaks = "3 months", 
                    date_labels = "%b") +
-  facet_wrap(~stn,ncol = 3)
+  facet_wrap(~station,ncol = 1)
   # ich habe an dieser Stelle theme(legend.position="none") entfernt
 
 
