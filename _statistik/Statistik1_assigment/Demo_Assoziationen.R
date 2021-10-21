d20207464a9e12a857af34c464789bdd90421875
smwidmer <wids@zhaw.ch>
2021-10-21 08:58:04 +0200

    hide statik part

--- _statistik/Statistik1_assigment/Demo_Assoziationen.R
+++ _statistik/Statistik1_assigment/Demo_Assoziationen.R
@@ -1,4 +1,4 @@
-knitr::purl("index.Rmd", "Demo_Assoziationen.R", documentation = 0)
+knitr::purl("aufgabe.Rmd", "Demo_Assoziationen.R", documentation = 0)
 
 ```{.r .distill-force-highlighting-css}
 ```
