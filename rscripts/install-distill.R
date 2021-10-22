53fba7909f905251067bc55adeba20cff4a7880c
Nils <rata@zhaw.ch>
2021-10-22 11:45:53 +0200

    add helper function

--- rscripts/install-distill.R
+++ rscripts/install-distill.R
@@ -1,4 +1,4 @@
 ## This line of code installs the newst version of distill. 
 ## If you are not Nils, you probabbly dont need to do this
 ## since renv::restore() will take care of this for you
-remotes::install_github("researchMethods-ZHAW/distill")
+remotes::install_github("ResearchMethods-ZHAW/distill")
